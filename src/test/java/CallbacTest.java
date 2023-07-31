fc04e9eb3c46c7774fbf0c8b2ab19a3606bf615c
EvgeniaZlobina <ZEA_6590@mail.ru>
2023-07-31 23:20:04 +0500

    Valid test

--- src/test/java/CallbacTest.java
+++ src/test/java/CallbacTest.java
@@ -1,15 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbacTest {
     private WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @AfterEach
@@ -19,7 +40,16 @@ public class CallbacTest {
     }
 
     @Test
-    void shouldTestSomething() {
-        throw new UnsupportedOperationException();
+    void RequestTest() {
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Захар");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79820000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actuaText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actuaText);
+
+
     }
+
 }
